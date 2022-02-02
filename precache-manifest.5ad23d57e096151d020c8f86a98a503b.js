5caae8ef4a713b542daec5d7429e308e5c51caf5
NEricN <neric@berkeley.edu>
2022-02-01 22:31:13 -0800

    Init commit

---
+++ precache-manifest.5ad23d57e096151d020c8f86a98a503b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657060069dd985ef111d8cafd3613a48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2cd25921167b15e7dbb1",
+    "url": "./static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ced0e9735587ecb71db9",
+    "url": "./static/js/2.661a3389.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.661a3389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2271765d6cf8a0a732fe",
+    "url": "./static/js/3.be4f24b8.chunk.js"
+  },
+  {
+    "revision": "2018fdf15d0379ed37a5325858bf6030",
+    "url": "./static/js/3.be4f24b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc94ecdffd1c62e3d29",
+    "url": "./static/js/4.347078a9.chunk.js"
+  },
+  {
+    "revision": "2cd25921167b15e7dbb1",
+    "url": "./static/js/main.22c26a45.chunk.js"
+  },
+  {
+    "revision": "b0f82c4da2e7b41fd94c",
+    "url": "./static/js/runtime-main.3643a58e.js"
+  }
+]);
